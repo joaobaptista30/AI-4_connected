f88b6477456d4ba2282a9e86f4494280c547454c
João Baptista <joao.baptista@outlook.pt>
2024-03-11 16:01:02 +0000

    Create MCTS.py

---
+++ MCTS.py
@@ -0,0 +1,28 @@
+from copy import deepcopy
+from Game4InLine import Game4InLine as G4
+
+class Node:
+
+    def __init__(self,game,parent):
+        self.game=deepcopy(game)
+        self.parent=parent
+        self.visited=0
+        self.wins=0
+        self.childs
+
+
+
+class MCTS:
+
+    def __init__(self,root):
+        self.root=deepcopy(root)
+
+
+    
+
+    def selection():
+
+
+        ...
+
+
